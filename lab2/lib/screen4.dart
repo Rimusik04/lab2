df513b1907c962b307f1fc8551d590e7a8f1ee34
Rimusik04 <106529684+Rimusik04@users.noreply.github.com>
2025-11-11 17:59:06 +0500

    last screen
    its all

--- lab2/lib/screen4.dart
+++ lab2/lib/screen4.dart
@@ -5,32 +5,40 @@ class Screen4 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    double SCwidth = MediaQuery.sizeOf(context).width;
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.white,
         actions: [Icon(Icons.favorite_border),SizedBox(width: 10,)],
       ),
       backgroundColor:Colors.white,
       body:
       SingleChildScrollView(
-        padding:EdgeInsets.all(10),
+        // padding:EdgeInsets.all(10),
         primary:true,
         child:Column(
+                  // padding:EdgeInsets.all(10),
+
           children: [
-            Image.asset("assets/Ca.png"),
-            Text("Junior UI Designer",style: TextStyle(fontSize: 30,fontWeight: FontWeight.bold),),
-            Text("Canva",style: TextStyle(fontSize: 20,fontWeight: FontWeight.w600),),
-            Text("Posted on 31 March",style: TextStyle(fontSize: 18,fontWeight: FontWeight.normal),),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Image.asset("assets/Ca.png",height: 100,),
+                Text("Junior UI Designer",style: TextStyle(fontSize: 30,fontWeight: FontWeight.bold),),
+                Text("Canva",style: TextStyle(fontSize: 20,fontWeight: FontWeight.w600),),
+                Text("Posted on 31 March",style: TextStyle(fontSize: 18,fontWeight: FontWeight.normal),),
+              ]
+            ),
             SizedBox(height: 20,),
             Container(
+              height: 5,
+              width: SCwidth,
               decoration: BoxDecoration(
-                // border: Border.all(
-                //   color: Colors.blue,
-                //   width: 
-                // )
-                color: Colors.amber
+                color:Color.fromARGB(255, 168, 225, 233),
               ),
               child: SizedBox(),
             ),
+            SizedBox(height: 20,),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
@@ -38,7 +46,7 @@ class Screen4 extends StatelessWidget {
                 children: [
                   Text("APPLY BEFORE"),
                   Text("03 June, 2022"),
-                  Text("      "),
+                SizedBox(height: 20,),
                   Text("SALARY RANGE"),
                   Text("40k - 60k/yearly"),
                 ],
@@ -47,41 +55,50 @@ class Screen4 extends StatelessWidget {
                 children: [
                   Text("JOB NATURE"),
                   Text("Full-time"),
-                  Text("      "),
+                SizedBox(height: 20,),
                   Text("JOB LOCATION"),
                   Text("Work from anywhere"),
                 ],
               ),            
             ],),
+            SizedBox(height: 20,),
             Container(
+              height: 5,
+              width: SCwidth,              
               decoration: BoxDecoration(
-                color: Colors.amber
+                color:Color.fromARGB(255, 168, 225, 233),
               ),
               child: SizedBox(),
             ),
-            Row(
+            SizedBox(height: 20,),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children:[
-                Text("Job Description"),
-                Text("      "),
-                Text("Can you bring creative human-centered ideas to life and make great things happen beyond what meets the eye? We believe in teamwork, fun, complex projects, diverse perspectives, and simple solutions. How about you? We're looking for a like-minded"),
-                Text("linkk"),
-                Text("See More"),
+                Text("             Job Description",style: TextStyle(fontSize: 22,fontWeight: FontWeight.w400),),
+                SizedBox(height: 20,),
+                Text("       Can you bring creative human-centered ideas to life and\n       make great things happen beyond what meets the eye? \n       We believe in teamwork, fun, complex projects, diverse\n       perspectives, and simple solutions. How about you?\n       We're looking for a like-minded"),
+                Text("                   "),
+                Text("       See More",style:TextStyle(color: Colors.green)),
               ],
             ),
+            SizedBox(height: 20,),
             Container(
+              height: 5,
+              width: SCwidth,
               decoration: BoxDecoration(
-                color: Colors.amber
+                color:Color.fromARGB(255, 168, 225, 233),
               ),
               child: SizedBox(),
             ),
-            Row(
+            SizedBox(height: 20,),
+            Column(
               children: [
                 Text("ROLES AND RESPONSIBILITIES"),
                 SizedBox(height: 10,),
-                Text("  • Drive the design process with cross-functional partners and design leads."),
-                Text("  • Design new experiences and patterns in a complex ecosystem and across platforms."),
-                Text("  • Partner with UX Research and Content Strategists through the design process."),
-                Text("  • Work closely with Product and Engineering to ensure a high quality implementation."),
+                Text("       • Drive the design process with cross-functional partners\n       and design leads."),
+                Text("       • Design new experiences and patterns in a complex   \n       ecosystem and across platforms."),
+                Text("       • Partner with UX Research and Content Strategists      \n       through the design process."),
+                Text("       • Work closely with Product and Engineering to ensure a \n       high quality implementation."),
               ],
             )
           ]
